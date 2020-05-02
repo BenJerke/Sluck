dd53d765e662f053ed57b22039ca0eb4e6d84b63
Robert Foster <robfos98@gmail.com>
2020-05-02 11:14:35 -0500

    connection

--- models/user.js
+++ models/user.js
@@ -26,8 +26,8 @@ const userSchema = new Schema ({
 
     },
 
-    directMessages {
+    directMessages: {
         type: Array
-    }
+    },
 
 });
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
 const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const http = require("http").Server(app);
+const io = require('socket.io')(http);
+//const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 
 // Middleware
@@ -11,12 +14,20 @@ app.use(express.json());
 // Static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+};
+
 // Add routes
 //app.use(routes);
 
 // Connect to DB
 //mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+io.on("connection", socket => console.log("A user connected."));
+io.listen(3002);
 
 // Start the API server
 app.listen(PORT, function() {
