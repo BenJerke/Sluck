9dd5071cdfd090be9c402a4c1bb57acba5a1df72
normalsled <appliedboredom@gmail.com>
2020-05-16 14:10:38 -0500

    we are now storing users and hashed passwords.

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,7 @@ const db = require ("../models")
 
 module.exports = {
     findAll: function(req, res) {
-        db.Channel
+        db.Post
             .find(req.query)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
