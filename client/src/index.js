bb31cb53d0d19459a3c28e873ffc091b5c0e335a
Robert Foster <robfos98@gmail.com>
2020-05-18 19:36:41 -0500

    password

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { CometChat } from '@cometchat-pro/chat';
 import './App.css';
-
-CometChat.init(process.env.PROJECT_THREE_KEY)
-.then(()=>{
-  console.log('Working');
-})
-.catch(() => {
-  console.log('RIP');
-})
 
 ReactDOM.render(<App />, document.getElementById('root'));
